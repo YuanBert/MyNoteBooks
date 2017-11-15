87d016b63ee4d197226b36147a57adc8c832244c
BertYuan <zgenyuan@hotmail.com>
2017-11-15 17:06:08 +0800

    first version with some bugs

---
+++ MotorTestPC/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace MotorTestPC
+{
+    partial class MotorCtrl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPortOfSenser = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chart1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.checkBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonStart);
+            this.splitContainer1.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.splitContainer1.Size = new System.Drawing.Size(895, 548);
+            this.splitContainer1.SplitterDistance = 689;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.Maximum = 500D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Maximum = 2300D;
+            chartArea1.AxisY.Minimum = 1500D;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            chartArea1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            chartArea1.BackImageTransparentColor = System.Drawing.Color.White;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Current";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Temperature";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(696, 542);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(24, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(69, 89);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(121, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "开始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(69, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "串口设置：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // serialPortOfSenser
+            // 
+            this.serialPortOfSenser.BaudRate = 115200;
+            this.serialPortOfSenser.PortName = "COM4";
+            this.serialPortOfSenser.ReceivedBytesThreshold = 6;
+            this.serialPortOfSenser.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortOfSenser_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(9, 141);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(168, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "显示芯片温度曲线（Blue）";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // MotorCtrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 548);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MotorCtrl";
+            this.Text = "MotorCtrl";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.IO.Ports.SerialPort serialPortOfSenser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ MotorTestPC/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace MotorTestPC
+{
+    public partial class MotorCtrl : Form
+    {
+        List<string>  uartName =new List<string>();
+        string[] sUartName; //= new string[10];
+        DataTable dt = new DataTable("Current_Temperture");
+        bool listeningFlag = false;
+        bool displayTempertureLineFlag = false;
+        long gTimeCnt = 0;
+        
+
+        public MotorCtrl()
+        {
+            InitializeComponent();
+
+            dt.Columns.Add("CurrentVale",typeof(Int16));
+            dt.Columns.Add("TempertureValue",typeof(Int16));
+            dt.Columns.Add("Date",System.Type.GetType("System.DateTime"));
+
+            buttonStart.Enabled = false;
+            sUartName = SerialPort.GetPortNames();
+            //foreach (string com in SerialPort.GetPortNames())
+            //{
+            //    uartName.Add(com);
+            //}
+            if (sUartName.Length > 0)
+            {
+                comboBox1.DataSource = sUartName;
+                comboBox1.SelectedIndex = 0;
+                buttonStart.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("没有发现串口设备！");
+            }
+            
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if ("开始" == buttonStart.Text)
+            {
+                serialPortOfSenser.PortName = comboBox1.SelectedItem.ToString();
+                try
+                {
+                    timer1.Start();
+                    serialPortOfSenser.Open();
+                    serialPortOfSenser.Write("S");
+                    buttonStart.Text = "停止";
+                    button1.BackColor = Color.Red;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (true == serialPortOfSenser.IsOpen)
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show(" 设备打开失败！");
+                }            
+            }
+            else
+            {
+                try
+                {
+                    timer1.Stop();
+                    serialPortOfSenser.Write("P");
+                    serialPortOfSenser.Close();
+                    chart1.Series[0].Points.Clear();
+                    chart1.Series[1].Points.Clear();
+                    gTimeCnt = 0;
+                    buttonStart.Text = "开始";
+                    button1.BackColor = Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+            }
+        }
+
+        private void serialPortOfSenser_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            if (false == serialPortOfSenser.IsOpen && listeningFlag)
+            {
+                MessageBox.Show("请打开某个串口","错误提示");
+                return;
+            }
+            byte[] ReDatas = new byte[6];
+            serialPortOfSenser.Read(ReDatas,0,6);
+            serialPortOfSenser.DiscardInBuffer();
+            if (ReDatas[0] != 0xFF || ReDatas[1] != 0xFF)
+            {
+                return;
+            }
+            int iCurrentValue = 0;
+            int tTempertureValue = 0;
+
+            iCurrentValue = ReDatas[2] << 8;
+            iCurrentValue += ReDatas[3];
+
+            tTempertureValue = ReDatas[4] << 8;
+            tTempertureValue += ReDatas[5];
+            this.BeginInvoke(new EventHandler(delegate
+            {
+                chart1.Series[0].Points.AddXY(gTimeCnt, iCurrentValue);
+                if(displayTempertureLineFlag)
+                {
+                    chart1.Series[1].Points.AddXY(gTimeCnt, tTempertureValue);
+                }
+                gTimeCnt++;
+
+                if (gTimeCnt > 500)
+                {
+                    gTimeCnt = 0;
+                    chart1.Series[0].Points.Clear();
+                    if (displayTempertureLineFlag)
+                    {
+                        chart1.Series[1].Points.Clear();
+                    }
+                    
+                }
+            }));
+
+            dt.Rows.Add(iCurrentValue, tTempertureValue, DateTime.Now);
+            if (dt.Rows.Count > 18000)
+            {
+                this.BeginInvoke(new EventHandler(delegate
+                {
+                    dt.Rows.Clear();
+                }));
+            }
+
+
+            //int i = 0;
+            //for (i = 0; i < ReDatas.Length; )
+            //{
+            //    iCurrentValue = ReDatas[i++] << 8;
+            //    iCurrentValue += ReDatas[i++];
+
+            //    tTempertureValue = ReDatas[i++] << 8;
+            //    tTempertureValue += ReDatas[i++];
+            //    dt.Rows.Add(iCurrentValue, tTempertureValue, DateTime.Now);
+
+
+            //}
+            //dt.Rows.Add(iCurrentValue,tTempertureValue,DateTime.Now);
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listeningFlag = true;
+            //DataTable tDt = new DataTable();
+            //int i = 0;
+            //int n = (dt.Rows.Count);
+            //int[] iCurrentArry = new int[n];
+            //int[] tTempertureArry = new int[n];
+
+            //for (i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    tTempertureArry[i] =Convert.ToInt16(dt.Rows[i][1].ToString());
+            //    iCurrentArry[i] = Convert.ToInt16(dt.Rows[i][0].ToString());
+            //}
+
+
+
+            //if (dt.Rows.Count > 8000)
+            //{
+            //    this.BeginInvoke(new EventHandler(delegate
+            //    {
+            //        dt.Rows.Clear();
+            //    })); 
+            //}
+
+            listeningFlag = false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                displayTempertureLineFlag = true;
+            }
+            else
+            {
+                displayTempertureLineFlag = false;
+                chart1.Series[1].Points.Clear();
+            }
+        }
+    }
+}
